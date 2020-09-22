9fcea22af812e2f107888f4e2f1245435457a6c2
notsobad <tomomichi.onishi@gmail.com>
2020-09-22 20:28:32 +0900

    add sidebar layout

--- pages/_app.js
+++ pages/_app.js
@@ -6,20 +6,26 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>Hangrila</title>
+        <title>Hangri-La</title>
       </Head>
-      <div className="max-w-screen-sm mx-auto px-2 sm:px-0">
+      <div className="max-w-screen-lg mx-auto px-2 sm:px-0">
         <header className="text-gray-700 body-font">
           <div className="py-5">
             <Link href="/">
               <a className="title-font text-gray-900">
-                <span className="text-lg">hangri-la</span>
+                <span className="text-lg">Hangri-La</span>
               </a>
             </Link>
           </div>
         </header>
 
-        <Component {...pageProps} />
+        <div className="flex flex-col sm:flex-row">
+          <div className="w-full md:w-2/3 mr-0 sm:mr-24 mb-24 sm:mb-0">
+            <Component {...pageProps} />
+          </div>
+          <div className="w-full md:w-1/3 bg-gray-300">
+          </div>
+        </div>
       </div>
     </>
   );
