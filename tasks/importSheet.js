ec8c398d4d64f2afa8c56bd7288297e24f57f3e7
notsobad <tomomichi.onishi@gmail.com>
2020-10-03 17:00:26 +0900

    import directly from sheet

--- tasks/importSheet.js
+++ tasks/importSheet.js
@@ -2,6 +2,7 @@
 // node ./tasks/importSheet.js [words|chars]
 
 const fs = require('fs');
+const fetch = require('node-fetch');
 const parse = require('csv-parse');
 const admin = require('firebase-admin');
 const serviceAccount = require("../cert.json");
@@ -13,50 +14,69 @@ admin.initializeApp({
 
 const target = process.argv[2];
 const batch = admin.firestore().batch();
-
-
-const importSheet = () => {
-  fs.createReadStream(`./tmp/${target}.csv`)
-    .pipe(parse({columns: true}))
-    .on('data', (row) => {
-      if(row.skip == 'true') { return; }
-
-      const params = Object.fromEntries(Object.entries(row).filter(([key, value]) => !['id', 'skip', 'created'].includes(key) )); //除外カラムを指定
-      params['updatedAt'] = new Date();
-      if(target == 'words') {
-        params['chars'] = row.id.split('');
-      }
-      if(!row.created) {
-        params['createdAt'] = new Date();
-      }
-
-      const ref = admin.firestore().collection(target).doc(row.id);
-      if(row.created) {
-        batch.update(ref, params);
-      }else {
-        batch.set(ref, params);
-      }
-      console.log(`import: ${row.id}`)
-    })
-    .on('end', () => {
-      batch.commit().then(() => {
-        console.log("import finished!")
-      });
-    });
+const sheetBaseUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5r3L8aVm_rFF02NfmaAgPq4ZEYrtPdcn1WcFBe4J1k36H6Aze3Bbvam9CIFxaxShDPYM7Qx5_btTG/pub?single=true&output=csv';
+const gids = {
+  chars: '0',
+  words: '1163711945',
+  phonological_changes: '471792344',
+  chars_changes: '1574865544',
 }
 
-const importCharsChanges = () => {
-  let results = {};
-  fs.createReadStream(`./tmp/${target}.csv`)
-    .pipe(parse({columns: true}))
-    .on('data', (row) => {
-      if(row.skip == 'true') { return; }
 
-      const params = Object.fromEntries(Object.entries(row).filter(([key, value]) => !['charId', 'skip'].includes(key) )); //除外カラムを指定
-      results[row.charId] = results[row.charId] || [];
-      results[row.charId].push(params);
+const importSheet = async () => {
+  const sheetUrl = `${sheetBaseUrl}&gid=${gids[target]}`;
+  const res = await fetch(sheetUrl);
+  const text = await res.text();
+
+  parse(text, {columns: true})
+    .on('readable', function(){
+      let row;
+      while (row = this.read()) {
+        if(row.skip == 'TRUE') { break; }
+
+        const params = Object.fromEntries(Object.entries(row).filter(([key, value]) => !['id', 'skip', 'created'].includes(key) )); //除外カラムを指定
+        params['updatedAt'] = new Date();
+        if(target == 'words') {
+          params['chars'] = row.id.split('');
+        }
+        if(!row.created) {
+          params['createdAt'] = new Date();
+        }
+
+        const ref = admin.firestore().collection(target).doc(row.id);
+        if(row.created) {
+          batch.update(ref, params);
+        }else {
+          batch.set(ref, params);
+        }
+        console.log(`imported: ${row.id}`)
+      }
     })
-    .on('end', () => {
+    .on('end', function(){
+      batch.commit().then(() => {
+        console.log("*** import finished! ***")
+      });
+    })
+}
+
+const importCharsChanges = async () => {
+  const sheetUrl = `${sheetBaseUrl}&gid=${gids[target]}`;
+  const res = await fetch(sheetUrl);
+  const text = await res.text();
+
+  let results = {};
+  parse(text, {columns: true})
+    .on('readable', function(){
+      let row;
+      while (row = this.read()) {
+        if(row.skip == 'TRUE') { break; }
+
+        const params = Object.fromEntries(Object.entries(row).filter(([key, value]) => !['charId', 'skip'].includes(key) )); //除外カラムを指定
+        results[row.charId] = results[row.charId] || [];
+        results[row.charId].push(params);
+      }
+    })
+    .on('end', function(){
       for (let charId in results) {
         const ref = admin.firestore().collection("chars").doc(charId);
         batch.update(ref, {
@@ -66,9 +86,9 @@ const importCharsChanges = () => {
         console.log(`set data for ${charId}`);
       }
       batch.commit().then(() => {
-        console.log("import finished!")
+        console.log("*** import finished! ***")
       });
-    });
+    })
 }
 
 
